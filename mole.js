8fe3bc7fecdb813835be6d1f7647ce1d99a592fa
Rahul3027 <rahus0763@gmail.com>
2025-02-24 19:12:03 +0530

    mole game

--- mole.js
+++ mole.js
@@ -3,39 +3,61 @@ let currPlaneTile;
 let score = 0;
 let gameOver = false;
 
-let play;
-
+let lastSpawned = null; // Track which one was last spawned
 
 window.onload = function() {
     setGame();
 }
 
 function setGame() {
-    // set up the grid for the game board in html
-    for(let i = 0 ; i < 9 ; i++) {
-      let tile = document.createElement("div");
-      tile.id = i.toString();
+    // Set up the grid for the game board in HTML
+    for(let i = 0; i < 9; i++) {
+        let tile = document.createElement("div");
+        tile.id = i.toString();
 
-      tile.addEventListener("click" ,setTile );
+        tile.addEventListener("click", setTile);
 
-      document.getElementById("board").appendChild(tile);
+        document.getElementById("board").appendChild(tile);
     }
-    
-    setInterval(setMole , 1000);
-    setInterval(setPlant,2000);
+
+    // Start the random spawn process
+    randomSpawn();
 }
 
 function getRandomTile() {
-    // to spawn the mole
+    // To spawn the mole or plant at a random tile
     let num = Math.floor(Math.random() * 9);
     return num.toString();
 }
 
-function setMole() {
-    if(gameOver){
+function randomSpawn() {
+    if(gameOver) {
         return;
     }
 
+    // Randomly decide whether to spawn a mole or a plant
+    if (Math.random() > 0.5) {
+        setMole();
+    } else {
+        setPlant();
+    }
+
+    // Randomize the spawn interval between 1 and 2 seconds
+    let randomTime = Math.random() * 1000 + 1000; // Random between 1000ms and 2000ms
+    setTimeout(randomSpawn, randomTime);
+}
+
+function setMole() {
+    if(gameOver) {
+        return;
+    }
+
+    // Remove plant if it exists
+    if(currPlaneTile) {
+        currPlaneTile.innerHTML = "";
+    }
+
+    // Remove mole if it exists
     if(currentMoleTile) {
         currentMoleTile.innerHTML = "";
     }
@@ -45,20 +67,35 @@ function setMole() {
 
     let num = getRandomTile();
     
+    // Make sure the mole doesn't spawn on a tile with the plant
     if(currPlaneTile && currPlaneTile.id == num) {
         return;
     }
 
     currentMoleTile = document.getElementById(num);
     currentMoleTile.appendChild(mole);
+
+    lastSpawned = "mole"; // Track that mole was last spawned
+
+    // Remove the mole after 1 second
+    setTimeout(() => {
+        if (currentMoleTile) {
+            currentMoleTile.innerHTML = "";
+        }
+    }, 1000); // 1 second duration for the mole
 }
 
 function setPlant() {
-
-    if(gameOver){
+    if(gameOver) {
         return;
     }
 
+    // Remove mole if it exists
+    if(currentMoleTile) {
+        currentMoleTile.innerHTML = "";
+    }
+
+    // Remove previous plant if present
     if(currPlaneTile) {
         currPlaneTile.innerHTML = "";
     }
@@ -68,22 +105,31 @@ function setPlant() {
 
     let num = getRandomTile();
 
+    // Make sure the plant doesn't spawn on a tile with the mole
     if(currentMoleTile && currentMoleTile.id == num) {
         return;
     }
 
     currPlaneTile = document.getElementById(num);
     currPlaneTile.appendChild(plant);
+
+    lastSpawned = "plant"; // Track that plant was last spawned
+
+    // Remove the plant after 1 second
+    setTimeout(() => {
+        if (currPlaneTile) {
+            currPlaneTile.innerHTML = "";
+        }
+    }, 1000); // 1 second duration for the plant
 }
 
 function setTile() {
     if(this == currentMoleTile) {
-        score+=10;
+        score += 10;
         document.getElementById("score").innerText = score.toString();
     }
-    else if ( this == currPlaneTile) {
-       document.getElementById("score").innerText = "Game Over:" + score.toString(); 
-       gameOver = true;
+    else if (this == currPlaneTile) {
+        document.getElementById("score").innerText = "Game Over: " + score.toString(); 
+        gameOver = true;
     }
 }
-
